ccda4c9415e4b481aaa244f8681ad1ad51e085b0
yeonjaeWon <139518183+yeonjaeWon@users.noreply.github.com>
2024-08-05 22:11:48 +0900

    Update views.py

--- controlapi/views.py
+++ controlapi/views.py
@@ -19,8 +19,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from apps.controlapi.models import Unofarm, Video
 import os
+import smtplib
+from email.mime.text import MIMEText
 
-#프로젝트 db
 username = 'uno'
 password = 'uno'
 dsn = 'localhost/xe'
@@ -39,7 +40,9 @@ Session = sessionmaker(bind=engine)
 db_session = Session()
 
 last_detection_time = None
+detection_interval = 0.5
 recording_start_time = None
+last_detection = None
 recording_duration = 9  # 녹화 시간 (초)
 save_path = 'C:/control/attach'  # 영상 저장 경로
 
@@ -64,10 +67,37 @@ def get_next_vno():
         return 1
     else:
         return last_vno[0] + 1
+    
+def send_danger_email(label, time):
+    # 이메일 설정
+    smtp_server = 'smtp.gmail.com'  # 이메일 서버 주소
+    smtp_port = 589  # 이메일 서버 포트
+    smtp_username = 'xxx@gmail.com'  # 발신자 이메일 주소
+    smtp_password = 'xxxxxxxxxxxxxx'  # 발신자 이메일 비밀번호
+    recipient = 'zzz@gmail.com'  # 수신자 이메일 주소
+
+    # 이메일 내용 작성
+    subject = '위험 객체 감지 알람'
+    body = f'위험 객체 ({label}) 감지 시간: {time}'
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = smtp_username
+    msg['To'] = recipient
+
+    try:
+        # 이메일 서버에 연결하고 이메일 전송
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(smtp_username, recipient, msg.as_string())
+        print('위험 객체 감지 알람 이메일을 성공적으로 전송했습니다.')
+    except Exception as e:
+        print('이메일 전송 중 오류가 발생했습니다:', str(e))
 
 def stream_gen(src):
-    global last_detection_time, recording_start_time
+    global last_detection_time, recording_start_time, last_detection
     fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    detected_labels = []
     video_writer = None
     try:
         # streamer 객체의 run() 메서드를 호출하여 지정된 소스에서 스트리밍을 시작한다.
@@ -90,30 +120,47 @@ def stream_gen(src):
             # 객체 감지 결과 얻기
             detections = results.pandas().xyxy[0]
             if not detections.empty:
+                current_time = datetime.now()
+                if last_detection is None or (current_time - last_detection).total_seconds() >= detection_interval:
+                    # 객체 감지 시간 업데이트
+                    last_detection = current_time
                 # 결과를 반복하며 객체 표시
-                for _, detection in detections.iterrows():
-                    x1, y1, x2, y2 = detection[['xmin', 'ymin', 'xmax', 'ymax']].astype(int).values
-                    label = detection['name']
-                    conf = detection['confidence']
-                    print(f'[Unofarm] Detected: {label} ({conf:.2f})')
+                    for _, detection in detections.iterrows():
+                        x1, y1, x2, y2 = detection[['xmin', 'ymin', 'xmax', 'ymax']].astype(int).values
+                        label = detection['name']
+                        conf = detection['confidence']
+                        print(f'[Unofarm] Detected: {label} ({conf:.2f})')
+
+                        # 감지된 객체를 리스트에 추가
+                        if label not in detected_labels:
+                            detected_labels.append(label)
+                            
+                        # 박스와 라벨 표시
+                        color = [int(c) for c in random.choice(range(256), size=3)]
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                        cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
                     
-                    # DB 저장 로직 추가
-                    current_time = datetime.now()
-                    if last_detection_time is None or (current_time - last_detection_time).total_seconds() >= 10:
+                    
+                    # DB 저장 시간 간격 확인
+                if last_detection_time is None or (current_time - last_detection_time).total_seconds() >= 10:
                         # 다음 dno 값 가져오기
                         next_dno = get_next_dno()
 
                         # Unofarm 객체 생성 및 저장
                         unofarm = Unofarm(
                             DNO=next_dno,
-                            D_LABEL=label,
+                            D_LABEL=', '.join(detected_labels),
                             D_TIME=current_time,
                             ID='nongjang',
-                            D_STATUS='Danger' if label in ['person', 'boar'] else 'Safe'
+                            D_STATUS='Danger' if any(label in ['person', 'boar'] for label in detected_labels) else 'Safe'
                         )
                         db_session.add(unofarm)
                         db_session.commit()
+                        if unofarm.D_STATUS == 'Danger':
+                            send_danger_email(unofarm.D_LABEL, unofarm.D_TIME)
                         last_detection_time = current_time
+                        detected_labels = []
                         
                         # 영상 녹화 시작
                         if video_writer is None:
@@ -123,11 +170,6 @@ def stream_gen(src):
                             print(video_path)
                             video_writer = cv2.VideoWriter(video_path, fourcc, 4.0, (1920,1080))
                     
-                    # 박스와 라벨 표시
-                    color = [int(c) for c in random.choice(range(256), size=3)]
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                    cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-            
             # 영상 녹화
             if video_writer is not None:
                 video_writer.write(frame)
@@ -174,5 +216,4 @@ def stream():
         )
     except Exception as e:
         print('[Unofarm] ', 'stream error : ', str(e))
-        
  
